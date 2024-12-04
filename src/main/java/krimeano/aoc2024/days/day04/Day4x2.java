f80e86442ad2d05b7d143f0a1dd2ae612716e86a
Sergii Paliutin <spalutin@flutterint.com>
2024-12-04 15:07:41 +0000

    day 4 2

--- src/main/java/krimeano/aoc2024/days/day04/Day4x2.java
+++ src/main/java/krimeano/aoc2024/days/day04/Day4x2.java
@@ -2,6 +2,8 @@ package krimeano.aoc2024.days.day04;
 
 import krimeano.aoc2024.days.my_lib.SolveDay;
 
+import java.util.ArrayList;
+
 public class Day4x2 extends SolveDay {
 
     public Day4x2(boolean verbose) {
@@ -10,6 +12,42 @@ public class Day4x2 extends SolveDay {
 
     @Override
     public int solve(String textInput) {
-        return 0;
+        int result = 0;
+
+        ArrayList<String> lines = getLines(textInput);
+
+        int height = lines.size();
+        if (height == 0) {
+            return result;
+        }
+
+        int width = lines.getFirst().length();
+        if (width == 0) {
+            return result;
+        }
+
+        for (int i = 1; i < height - 1; i++) {
+            for (int j = 1; j < width - 1; j++) {
+                if (lines.get(i).charAt(j) == 'A') {
+                    if (verbose) {
+                        System.out.println("A is found at " + i + ", " + j);
+                    }
+                    if (
+                            ((lines.get(i - 1).charAt(j - 1) == 'M') && (lines.get(i + 1).charAt(j + 1) == 'S')
+                                    || (lines.get(i - 1).charAt(j - 1) == 'S') && (lines.get(i + 1).charAt(j + 1) == 'M'))
+                                    && ((lines.get(i - 1).charAt(j + 1) == 'M') && (lines.get(i + 1).charAt(j - 1) == 'S')
+                                    || (lines.get(i - 1).charAt(j + 1) == 'S') && (lines.get(i + 1).charAt(j - 1) == 'M'))
+
+                    ) {
+                        if (verbose) {
+                            System.out.println("X-MAS is found at " + i + ", " + j);
+                        }
+                        result += 1;
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }
---
+++ src/tests/java/krimeano/aoc2024/days/day04/Day4x2Test.java
@@ -0,0 +1,26 @@
+package krimeano.aoc2024.days.day04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day4x2Test {
+    String TEST_INPUT = """
+                     MMMSXXMASM
+                     MSAMXMSMSA
+                     AMXSXMAAMM
+                     MSAMASMSMX
+                     XMASAMXAMM
+                     XXAMMXXAMA
+                     SMSMSASXSS
+                     SAXAMASAAA
+                     MAMMMXMMMM
+                     MXMXAXMASX
+            """;
+
+    @Test
+    void solve() {
+        assertEquals(9, new Day4x2(true).solve(TEST_INPUT));
+    }
+
+}
