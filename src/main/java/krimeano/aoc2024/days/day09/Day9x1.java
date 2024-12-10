747122e214ec9cc00f0801d4a58d561bc521358f
Sergii Paliutin <spalutin@flutterint.com>
2024-12-10 16:49:10 +0000

    day 9 1

--- src/main/java/krimeano/aoc2024/days/day09/Day9x1.java
+++ src/main/java/krimeano/aoc2024/days/day09/Day9x1.java
@@ -2,6 +2,8 @@ package krimeano.aoc2024.days.day09;
 
 import krimeano.aoc2024.days.my_lib.SolveDay;
 
+import java.util.ArrayList;
+
 public class Day9x1 extends SolveDay {
     public Day9x1(boolean verbose) {
         super(verbose);
@@ -10,15 +12,69 @@ public class Day9x1 extends SolveDay {
     @Override
     public int solve(String textInput) {
         String line = textInput.trim();
+//        long result = solveBruteforce(line);
+        long result = solveOptimized(line);
+        System.out.println(result);
+        return (int) result;
+    }
+
+    protected long solveBruteforce(String line) {
+        long result = 0;
+        ArrayList<Integer> disk = new ArrayList<>();
+        int ix = 0;
+        boolean mode = true;
+        for (char c : line.toCharArray()) {
+            int width = c - '0';
+            if (verbose) {
+                System.out.println(ix + " " + c + " " + width + " " + mode);
+            }
+            for (byte i = 0; i < width; i++) {
+                disk.add(mode ? ix : -1);
+            }
+            if (!mode) {
+                ix += 1;
+            }
+            mode = !mode;
+        }
+        if (verbose) {
+            System.out.println(disk);
+        }
+
+        int cursor = 0;
+
+        while (cursor < disk.size()) {
+            int value = disk.get(cursor);
+            System.out.println(cursor + "/" + disk.size() + "="+ value);
+            System.out.println(disk.subList(disk.size() - 20, disk.size()));
+            if (value != -1) {
+                result += (long) value * (long) cursor;
+                cursor++;
+                continue;
+            }
+            while (value == -1 && cursor < disk.size()) {
+                value = disk.removeLast();
+            }
+            if (value == -1) {
+                break;
+            }
+            result += (long) value * (long) cursor;
+            disk.set(cursor++, value);
+        }
+        System.out.println(disk.subList(disk.size() - 20, disk.size()));
+
+        return result;
+    }
+
+    protected long solveOptimized(String line) {
         long result = 0;
         int left_cursor = 0;
         int right_cursor = line.length() - 1;
         right_cursor -= (right_cursor % 2);
-        int left_width = line.charAt(left_cursor) - '0';
-        int right_width = line.charAt(right_cursor) - '0';
-        int position = 0;
-        int width;
-        int ix;
+        long left_width = line.charAt(left_cursor) - '0';
+        long right_width = line.charAt(right_cursor) - '0';
+        long position = 0;
+        long width;
+        long ix;
         long volume;
         while (left_cursor < right_cursor) {
             if (left_cursor % 2 == 0) {
@@ -53,7 +109,6 @@ public class Day9x1 extends SolveDay {
             volume = ix * (position * width + width * (width - 1) / 2);
             result += volume;
         }
-        System.out.println(result);
-        return (int) result;
+        return result;
     }
 }
