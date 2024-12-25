a196649a95132f7c5a8434d862131d15b4782e6b
Sergii Paliutin <spalutin@flutterint.com>
2024-12-25 01:06:02 +0000

    day 24 1

--- src/main/java/krimeano/aoc2024/days/day24/Day24x2.java
+++ src/main/java/krimeano/aoc2024/days/day24/Day24x2.java
@@ -1,9 +1,14 @@
 package krimeano.aoc2024.days.day24;
 
+import krimeano.aoc2024.days.my_lib.SolveDay;
+
 import java.util.*;
 
-public class Day24x2 extends Day24x1 {
-    Map<String, Set<String>> parentInputs;
+public class Day24x2 extends SolveDay {
+    List<String> inputs;
+    Map<String, Set<String>> parentsMap;
+    Map<String, Set<String>> childrenMap;
+    List<List<String>> tiers;
 
     public Day24x2(boolean verbose) {
         super(verbose);
@@ -12,50 +17,102 @@ public class Day24x2 extends Day24x1 {
     @Override
     public int solve(String textInput) {
         readData(textInput);
-//        swapWires("z26","dfp");
-//        swappedWires = new HashMap<>();
-//        swappedWires.put("z26", "dfp");
-//        swappedWires.put("dfp", "z26");
         sortGates();
-        connectWires();
+//        for (String key : parentsMap.keySet()) {
+//            System.out.println(key + " < " + parentsMap.get(key));
+//        }
+//        for (String key : childrenMap.keySet()) {
+//            System.out.println(key + " > " + childrenMap.get(key));
+//        }
         return -1;
     }
 
-    private void swapWires(String keyX, String keyY) {
-        List<String> tmp = gatesMap.get(keyX);
-        gatesMap.put(keyX, gatesMap.get(keyY));
-        gatesMap.put(keyY, tmp);
+    protected void readData(String textInput) {
+        inputs = new ArrayList<>();
+        parentsMap = new HashMap<>();
+        childrenMap = new HashMap<>();
+        boolean isInitWires = true;
+
+        for (String line : getLines(textInput, true)) {
+            if (line.isEmpty()) {
+                isInitWires = false;
+                continue;
+            }
+            if (isInitWires) {
+                inputs.add(line.split(": ")[0]);
+            } else {
+                String[] parts = line.split(" -> ");
+                String[] tokens = parts[0].split(" ");
+                String keyZ = parts[1];
+                String keyX = tokens[0];
+                String keyY = tokens[2];
+
+                parentsMap.put(keyZ, new HashSet<>());
+                parentsMap.get(keyZ).add(keyX);
+                parentsMap.get(keyZ).add(keyY);
+
+                childrenMap.putIfAbsent(keyX, new HashSet<>());
+                childrenMap.get(keyX).add(keyZ);
+
+                childrenMap.putIfAbsent(keyY, new HashSet<>());
+                childrenMap.get(keyY).add(keyZ);
+            }
+        }
+
+        inputs.sort(Comparator.comparing(x -> x.substring(1)));
     }
 
-    @Override
-    protected void connectWires() {
-
-        parentInputs = new HashMap<>();
-
-        for (String key : wires.keySet()) {
-            parentInputs.put(key, new HashSet<>());
-            parentInputs.get(key).add(key);
-        }
-
-        for (String keyZ : sortedGateWires) {
-            List<String> gate = getGate(keyZ);
-
-            String keyX = gate.getFirst();
-            String keyY = gate.getLast();
-
-            Set<String> parentsX = parentInputs.getOrDefault(keyX, new HashSet<>());
-            Set<String> parentsY = parentInputs.getOrDefault(keyY, new HashSet<>());
-            Set<String> parentsZ = new HashSet<>(parentsX);
-            parentsZ.addAll(parentsY);
-            parentInputs.put(keyZ, parentsZ);
-            List<String> parentsSorted = new ArrayList<>(parentsZ);
-            parentsSorted.sort(Comparator.naturalOrder());
-            if (keyZ.startsWith("z")) {
-                System.out.println(keyZ + " parents: " + parentsZ.size() + " " + parentsSorted);
+    protected void sortGates() {
+        tiers = new ArrayList<>();
+        int cursor = 0;
+        List<String> lastTier = new ArrayList<>();
+        Set<String> visitedWires = new HashSet<>();
+        while (!lastTier.isEmpty() || cursor < inputs.size()) {
+            List<String> newTier = new ArrayList<>();
+            Set<String> newVisitedWires = new HashSet<>();
+            if (lastTier.isEmpty()) {
+                String x = inputs.get(cursor++);
+                String y = inputs.get(cursor++);
+                newTier.add(x);
+                newVisitedWires.add(x);
+                newTier.add(y);
+                newVisitedWires.add(y);
             }
-
+            for (String wire : lastTier) {
+                if (childrenMap.containsKey(wire)) {
+                    for (String child : childrenMap.get(wire)) {
+                        if (!newTier.contains(child)) {
+                            boolean bothParentsVisited = true;
+                            if (parentsMap.containsKey(child)) {
+                                for (String parent : parentsMap.get(child)) {
+                                    bothParentsVisited = bothParentsVisited && visitedWires.contains(parent);
+                                }
+                            }
+                            if (bothParentsVisited) {
+                                newTier.add(child);
+                                newVisitedWires.add(child);
+                            }
+                        }
+                    }
+                }
+            }
+            if (!newTier.isEmpty()) {
+                tiers.add(newTier);
+                visitedWires.addAll(newVisitedWires);
+            }
+            lastTier = newTier;
         }
 
-        System.out.println(gatesMap.size());
+        for (List<String> tier : tiers) {
+            for (String wire : tier) {
+                if (parentsMap.containsKey(wire)) {
+                    System.out.print(parentsMap.get(wire) + " > ");
+                } else {
+                    System.out.print("             ");
+                }
+                System.out.print(wire + "        ");
+            }
+            System.out.println();
+        }
     }
 }
